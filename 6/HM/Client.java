eb1ec092e3ce5797646570879a5ec4802c56e9c3
Johnny2825 <35284499+Johnny2825@users.noreply.github.com>
2020-04-13 19:49:50 +0900

    HM6

---
+++ 6/HM/Client.java
@@ -0,0 +1,90 @@
+package HM;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 8189;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Client() {
+        try {
+            openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openConnection() throws IOException {
+        socket = new Socket(SERVER_ADDR, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Для закрытия соединения с сервером и завершение программы введите '/End'");
+
+        new Thread(() -> {
+            try {
+                while(true){
+                    String str = in.readUTF();
+                    if(str.equals("/End")){
+                        System.out.println("Сервер закрыл соединение");
+                        close(in, out, socket);
+                        System.exit(0);
+                    } else {
+                        System.out.println("Server: " + str);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true){
+                    String str = inputFromKeyboard();
+                    if(!str.equals("")){out.writeUTF(str); }
+                    if(str.equals("/End")){
+                        System.out.println("Вы закрыли соединение");
+                        close(in, out, socket);
+                        System.exit(0);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    public String inputFromKeyboard(){
+        String str = null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            str = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    private void close(Closeable... objects){
+        for(Closeable o : objects){
+            try {
+                if (o != null) {
+                    o.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+
+}
